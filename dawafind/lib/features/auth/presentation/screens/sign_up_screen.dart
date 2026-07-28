69887b58aaddcbc0c61d07a0cd3369ddd5ac87fb
Fadhili-code <f.lumumba@alustudent.com>
2026-07-28 09:23:13 +0200

    feat: add sign up screen with form validation

---
+++ dawafind/lib/features/auth/presentation/screens/sign_up_screen.dart
@@ -0,0 +1,126 @@
+import 'package:flutter/material.dart';
+import '../../../../core/constants/app_colors.dart';
+import '../../../../core/constants/app_strings.dart';
+import '../../../../core/constants/app_routes.dart';
+import '../../../../core/utils/validators.dart';
+
+class SignUpScreen extends StatefulWidget {
+  const SignUpScreen({super.key});
+
+  @override
+  State<SignUpScreen> createState() => _SignUpScreenState();
+}
+
+class _SignUpScreenState extends State<SignUpScreen> {
+  final _formKey = GlobalKey<FormState>();
+  bool _obscurePassword = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: AppColors.white,
+      appBar: AppBar(
+        backgroundColor: AppColors.white,
+        elevation: 0,
+        iconTheme: const IconThemeData(color: AppColors.darkText),
+      ),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.symmetric(horizontal: 24),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(AppStrings.createAccount,
+                  style: const TextStyle(
+                      fontSize: 28,
+                      fontWeight: FontWeight.bold,
+                      color: AppColors.darkText)),
+              const SizedBox(height: 24),
+              _field(label: AppStrings.fullName,
+                  icon: Icons.person_outline,
+                  validator: Validators.required),
+              _field(label: AppStrings.phoneNumber,
+                  icon: Icons.phone_outlined,
+                  validator: Validators.phone),
+              _field(
+                label: AppStrings.password,
+                icon: Icons.lock_outline,
+                obscure: _obscurePassword,
+                validator: Validators.password,
+                suffix: IconButton(
+                  icon: Icon(_obscurePassword
+                      ? Icons.visibility_outlined
+                      : Icons.visibility_off_outlined),
+                  onPressed: () => setState(
+                      () => _obscurePassword = !_obscurePassword),
+                ),
+              ),
+              _field(label: AppStrings.confirmPassword,
+                  icon: Icons.lock_outline,
+                  obscure: true,
+                  validator: Validators.required),
+              const SizedBox(height: 16),
+              SizedBox(
+                width: double.infinity,
+                height: 56,
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: AppColors.primaryGreen,
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(28)),
+                  ),
+                  onPressed: () {
+                    if (_formKey.currentState!.validate()) {
+                      Navigator.pushNamed(context, AppRoutes.homePatient);
+                    }
+                  },
+                  child: Text(AppStrings.createAccount,
+                      style: const TextStyle(
+                          color: AppColors.white,
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold)),
+                ),
+              ),
+              const SizedBox(height: 16),
+              Center(
+                child: TextButton(
+                  onPressed: () =>
+                      Navigator.pushNamed(context, AppRoutes.login),
+                  child: const Text('Already have an account? Log In',
+                      style:
+                          TextStyle(color: AppColors.primaryGreen)),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _field({
+    required String label,
+    required IconData icon,
+    required String? Function(String?) validator,
+    bool obscure = false,
+    Widget? suffix,
+  }) =>
+      Padding(
+        padding: const EdgeInsets.only(bottom: 16),
+        child: TextFormField(
+          obscureText: obscure,
+          validator: validator,
+          decoration: InputDecoration(
+            labelText: label,
+            prefixIcon: Icon(icon, size: 20),
+            suffixIcon: suffix,
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(12),
+              borderSide:
+                  const BorderSide(color: AppColors.lightGreyBorder),
+            ),
+          ),
+        ),
+      );
+}
