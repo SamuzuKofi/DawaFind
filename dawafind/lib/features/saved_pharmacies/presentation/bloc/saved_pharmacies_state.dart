8308662a61c460f7dc63d6349ad4dc21cfd67e1b
Larry-Sentore <l.sentore@alustudent.com>
2026-07-30 15:38:27 +0200

    added the bloc implementation for the saved pharmacies page

---
+++ dawafind/lib/features/saved_pharmacies/presentation/bloc/saved_pharmacies_state.dart
@@ -0,0 +1,17 @@
+part of 'saved_pharmacies_bloc.dart';
+
+abstract class SavedPharmaciesState {}
+
+class SavedPharmaciesInitial extends SavedPharmaciesState {}
+
+class SavedPharmaciesLoading extends SavedPharmaciesState {}
+
+class SavedPharmaciesReady extends SavedPharmaciesState {
+  SavedPharmaciesReady({required this.pharmacies});
+  final List<dynamic> pharmacies;
+}
+
+class SavedPharmaciesError extends SavedPharmaciesState {
+  SavedPharmaciesError({required this.message});
+  final String message;
+}
