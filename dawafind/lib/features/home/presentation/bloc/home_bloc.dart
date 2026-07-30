55b95582fcb08170416d45124cdf6572c63f83c1
Larry-Sentore <l.sentore@alustudent.com>
2026-07-30 15:36:36 +0200

    added the Bloc implementation for the home screen

--- dawafind/lib/features/home/presentation/bloc/home_bloc.dart
+++ dawafind/lib/features/home/presentation/bloc/home_bloc.dart
@@ -1,1 +1,18 @@
-// HomeBloc
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+part 'home_event.dart';
+part 'home_state.dart';
+
+class HomeBloc extends Bloc<HomeEvent, HomeState> {
+  HomeBloc() : super(HomeInitial()) {
+    on<HomeLoaded>(_onLoaded);
+  }
+
+  Future<void> _onLoaded(
+    HomeLoaded event,
+    Emitter<HomeState> emit,
+  ) async {
+    emit(HomeLoading());
+    // call PreferencesService.getUserType() then emit HomeReady
+  }
+}
